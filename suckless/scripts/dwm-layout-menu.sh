65203cbf5334e2eea3810d79b0f7a196f8a3a509
JustAGuyLinux <jag@justaguylinux.com>
2025-03-07 16:38:01 -0500

    Update dwm-layout-menu.sh

--- suckless/scripts/dwm-layout-menu.sh
+++ suckless/scripts/dwm-layout-menu.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Define your layouts with keybinds and descriptions
 layouts=(
     "[\\] Dwindle              Shift+Control+1"    
     "|M| Centered Master       Shift+Control+2"    
@@ -16,25 +15,23 @@ layouts=(
     "HHH Grid                  Shift+Control+="    
 )
 
-# Show the layout menu with rofi
-selected=$(printf "%s\n" "${layouts[@]}" | rofi -dmenu -i -p "Select Layout" -line-padding 4 -hide-scrollbar -theme ~/.config/suckless/rofi/keybinds.rasi) 
+selected=$(printf "%s\n" "${layouts[@]}" | rofi -dmenu -i -p "Select Layout" -line-padding 4 -hide-scrollbar -theme ~/.config/suckless/rofi/keybinds.rasi)
 
-# Extract the layout name (removing the keybind and description parts for matching)
-layout_name=$(echo "$selected" | awk '{$1=$2=""; print substr($0,3)}' | sed 's/^ *//')
+# Extract just the first two fields (layout name)
+layout_name=$(echo "$selected" | cut -d ' ' -f 1-2)
 
-# Map selection to keybinds
 case "$layout_name" in
     "[\\] Dwindle") xdotool key shift+control+1 ;;
-    "|M| Centered Master") xdotool key shift+control+2 ;;
+    "|M| Centered") xdotool key shift+control+2 ;;
     "[]= Tile") xdotool key shift+control+3 ;;
-    "[C] Column Layout") xdotool key shift+control+4 ;;
+    "[C] Column") xdotool key shift+control+4 ;;
     ">< Floating") xdotool key shift+control+5 ;;
     "TTT Bstack") xdotool key shift+control+6 ;;
-    "### N-Row Grid") xdotool key shift+control+7 ;;
+    "### N-Row") xdotool key shift+control+7 ;;
     "H[] Deck") xdotool key shift+control+8 ;;
-    "::: Gapless Grid") xdotool key shift+control+9 ;;
+    "::: Gapless") xdotool key shift+control+9 ;;
     "[@] Spiral") xdotool key shift+control+0 ;;
     "[M] Monocle") xdotool key shift+control+minus ;;
     "HHH Grid") xdotool key shift+control+equal ;;
-    *) exit 0 ;;  # No selection or cancel
+    *) exit 0 ;;  # Exit on cancel
 esac
