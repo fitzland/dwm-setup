bf30c1141ab128049fc40bbf6b70d1328a7d471a
JustAGuyLinux <jag@justaguylinux.com>
2025-03-07 09:30:34 -0500

    Update dwm-layout-menu.sh

--- suckless/scripts/dwm-layout-menu.sh
+++ suckless/scripts/dwm-layout-menu.sh
@@ -1,32 +1,34 @@
-
 #!/bin/bash
 
-# Define your layouts and their corresponding keys
+# Define your layouts with keybinds and descriptions
 layouts=(
-    "[\\] Dwindle" # Layout 0 -> Shift+Control+1
-    "|M| Centered Master" # Layout 1 -> Shift+Control+2
-    "[]= Tile" # Layout 2 -> Shift+Control+3
-    "[C] Column Layout" # Layout 3 -> Shift+Control+4
-    "><> Floating" # Layout 4 -> Shift+Control+5
-    "TTT Bstack" # Layout 5 -> Shift+Control+6
-    "### N-Row Grid" # Layout 6 -> Shift+Control+7
-    "H[] Deck" # Layout 7 -> Shift+Control+8
-    "::: Gapless Grid" # Layout 8 -> Shift+Control+9
-    "[@] Spiral" # Layout 9 -> Shift+Control+0
-    "[M] Monocle" # Layout 10 -> Shift+Control+minus
-    "HHH Grid" # Layout 11 -> Shift+Control+equal
+    "[\\] Dwindle              Shift+Control+1"    
+    "|M| Centered Master       Shift+Control+2"    
+    "[]= Tile                  Shift+Control+3"    
+    "[C] Column Layout         Shift+Control+4"    
+    ">< Floating               Shift+Control+5"    
+    "TTT Bstack                Shift+Control+6"    
+    "### N-Row Grid            Shift+Control+7"    
+    "H[] Deck                  Shift+Control+8"    
+    "::: Gapless Grid          Shift+Control+9"    
+    "[@] Spiral                Shift+Control+0"    
+    "[M] Monocle               Shift+Control+-"    
+    "HHH Grid                  Shift+Control+="    
 )
 
 # Show the layout menu with rofi
 selected=$(printf "%s\n" "${layouts[@]}" | rofi -dmenu -i -p "Select Layout" -line-padding 4 -hide-scrollbar -theme ~/.config/suckless/rofi/keybinds.rasi) 
 
+# Extract the layout name (removing the keybind and description parts for matching)
+layout_name=$(echo "$selected" | awk '{$1=$2=""; print substr($0,3)}' | sed 's/^ *//')
+
 # Map selection to keybinds
-case "$selected" in
+case "$layout_name" in
     "[\\] Dwindle") xdotool key shift+control+1 ;;
     "|M| Centered Master") xdotool key shift+control+2 ;;
     "[]= Tile") xdotool key shift+control+3 ;;
     "[C] Column Layout") xdotool key shift+control+4 ;;
-    "><> Floating") xdotool key shift+control+5 ;;
+    ">< Floating") xdotool key shift+control+5 ;;
     "TTT Bstack") xdotool key shift+control+6 ;;
     "### N-Row Grid") xdotool key shift+control+7 ;;
     "H[] Deck") xdotool key shift+control+8 ;;
