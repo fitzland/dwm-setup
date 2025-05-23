f39c7491b46952243c70d4c52a2109d06aa217e2
JustAGuyLinux <jag@justaguylinux.com>
2025-05-23 14:50:22 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -266,15 +266,10 @@ install_displaymanager() {
     run_butterscript "system/install_lightdm.sh"
 }
 
-# ============================================
 # Replace .bashrc
 # ============================================
 replace_bashrc() {
-    read -p "Replace your .bashrc with justaguylinux .bashrc? (y/n) " response
-    if [[ "$response" =~ ^[Yy]$ ]]; then
-        wget -O ~/.bashrc https://raw.githubusercontent.com/drewgrif/jag_dots/main/.bashrc
-        source ~/.bashrc
-    fi
+    run_butterscript "system/add_bashrc.sh"
 }
 
 # ============================================
